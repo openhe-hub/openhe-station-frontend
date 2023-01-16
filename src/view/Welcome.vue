8e9c099e6802a170023e5ed18d5ab180ec06a6bb
openhe <625529334@qq.com>
2023-01-17 01:07:16 +0800

    1-16-upd: typing effect

--- src/view/Welcome.vue
+++ src/view/Welcome.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="container">
     <div id="center" class="blur-container">
-      <p id="welcome_title">Openhe's Station</p>
-      <el-button type="primary">
+      <p class="welcome_title">{{ typewriter[0] }}</p>
+      <p class="welcome_title">{{ typewriter[1] }}</p>
+      <el-button type="primary" size="large">
         Enter
-        <el-icon class="el-icon--right"><DArrowRight /></el-icon>
+        <el-icon class="el-icon--right">
+          <DArrowRight/>
+        </el-icon>
       </el-button>
-      <el-button type="success" >
+      <el-button type="success" size="large">
         Search
-        <el-icon class="el-icon--right"><Search /></el-icon>
+        <el-icon class="el-icon--right">
+          <Search/>
+        </el-icon>
       </el-button>
       <p>[Sentence]</p>
     </div>
@@ -17,6 +22,50 @@
 
 <script setup>
 import {DArrowRight, Search} from "@element-plus/icons-vue";
+import {onMounted, reactive, ref} from "vue";
+
+// typewriter
+const typewriter = reactive(['', '']);
+let i = 0, timer = 0, curr = 0, isFlash = false;
+const str = ['Welcome to', 'openhe-station.'];
+
+const typing = () => {
+  if (curr < str.length) {
+    typewriter[curr] = str[curr].slice(0, (i++) + 1) + (i === str[curr].length ? "" : "|");
+    if (i === str[curr].length) {
+      curr++;
+      i = 0;
+    }
+    timer = setTimeout(() => {
+      typing()
+    }, 120);
+  } else {
+    clearTimeout(timer);
+  }
+}
+
+const flashing = () => {
+  if (curr === str.length) {
+    console.log(typewriter[1]);
+    if (!isFlash) {
+      typewriter[1] += '|'
+      isFlash = true;
+    } else {
+      typewriter[1] = typewriter[1].slice(0, typewriter[1].length - 1);
+      isFlash = false;
+    }
+  }
+  timer = setTimeout(() => {
+    flashing()
+  }, 400)
+}
+
+onMounted(() => {
+  typing();
+  flashing()
+})
+
+
 </script>
 
 <style scoped>
@@ -40,18 +89,18 @@ div#center {
   padding: 5% 2%;
 }
 
-#welcome_title{
+.welcome_title {
   font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
   'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
-  font-size: 20px;
+  font-size: 25px;
 }
 
-div.blur-container{
+div.blur-container {
   background: rgba(255, 255, 255, 0.2);
   -webkit-backdrop-filter: blur(8px);
   backdrop-filter: blur(8px);
   border: 0px solid black;
   border-radius: 10px;
-  box-shadow:inset 0 0 6px rgba(255, 255, 255, 0.2);
+  box-shadow: inset 0 0 6px rgba(255, 255, 255, 0.2);
 }
 </style>
