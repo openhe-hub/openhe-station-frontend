95151a49b697fcf0e2cfbdad1dfbad75b932c6f5
openhe <625529334@qq.com>
2023-01-18 22:44:43 +0800

    1-18-upd: finish Timeline.vue

--- src/components/index/Timeline.vue
+++ src/components/index/Timeline.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="timeline-container">
     <el-row>
-      <el-col :span="8" v-for="i in [0,1]">
+      <el-col :span="10" v-for="(timeline,idx) in timelines" :key="idx" id="timeline-container">
         <el-timeline>
           <el-timeline-item
-              v-for="(activity, index) in activities"
+              v-for="(activity, index) in timeline"
               :key="index"
               :icon="activity.icon"
               :type="activity.type"
@@ -17,33 +17,62 @@
           </el-timeline-item>
         </el-timeline>
       </el-col>
+      <!--      <el-col :span="10">-->
+
+      <!--      </el-col>-->
     </el-row>
 
   </div>
 </template>
 
 <script setup>
-import { MoreFilled } from '@element-plus/icons-vue'
+import {SuccessFilled, Calendar, Sunny} from '@element-plus/icons-vue'
 
-const activities = [
+const timeline1 = [
   {
-    content: 'Custom icon',
-    timestamp: '2018-04-12 20:46',
+    content: 'Project Finished',
+    timestamp: '2023-06-01',
     size: 'large',
     type: 'primary',
-    icon: MoreFilled,
+    icon: SuccessFilled,
   },
   {
-    content: 'Custom color',
-    timestamp: '2018-04-03 20:46',
+    content: 'Project Init',
+    timestamp: '2023-01-16',
     color: '#0bbd87',
   },
   {
-    content: 'Default node',
-    timestamp: '2018-04-03 20:46',
+    content: 'Project Setup',
+    timestamp: '2022-12-15',
   },
 ]
+
+const timeline2 = [
+  {
+    content: 'Summer Term',
+    timestamp: '2023-05-10',
+    size: 'large',
+    type: 'danger',
+    icon: Sunny,
+  },
+  {
+    content: 'Spring Term',
+    timestamp: '2023-02-13',
+    color: '#b1d5c8',
+  },
+  {
+    content: 'Winter Holiday',
+    timestamp: '2022-12-17',
+  },
+]
+
+const timelines = [timeline1, timeline2];
 </script>
 
 <style scoped>
+div#timeline-container {
+  margin: 10px;
+  padding: 10px;
+}
+
 </style>
